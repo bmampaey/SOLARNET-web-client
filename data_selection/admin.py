eea161a569e9d09044bd9cfa0be967fe9f61bbf6
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-02-10 12:12:01 +0100

    defined admin for models

--- data_selection/admin.py
+++ data_selection/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from data_selection.models import UserDataSelection, DataSelection
+
+@admin.register(UserDataSelection)
+class UserDataSelectionAdmin(admin.ModelAdmin):
+	'''Admin class for the UserDataSelection model'''
+	list_display = ["user", "name", "created"]
+	list_filter = ["user__username"]
+
+
+@admin.register(DataSelection)
+class DataSelectionAdmin(admin.ModelAdmin):
+	'''Admin class for the DataSelection model'''
+	list_display = ["user_data_selection", "dataset", "created"]
+	list_filter = ["dataset"]

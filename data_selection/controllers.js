ccac3c4a3e1ccda4608d888ecb1567a401d22f8c
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-04-08 15:04:18 +0200

    tentative of code organization

--- data_selection/controllers.js
+++ data_selection/controllers.js
@@ -1,62 +1,31 @@
-angular.module('dataSelectionApp')
-.controller('UserDataSelectionController', function($scope, UserDataSelection, DataSelection, $uibModal, bsLoadingOverlayService) {
+angular
+.module('dataSelectionApp')
+.controller('DataSelectionController', function($uibModal, data_selections) {
 	
-	console.log("UserDataSelectionController scope", $scope.$id);
+	var vm = this;
 	
-	$scope.data_selections = [];
+	vm.data_selections = data_selections;
+	vm.open_detail = open_detail;
 	
-	// function to load the table
-	$scope.load_user_data_selections = function(){
-		console.log("Updating user data selections");
-		
-		// set the loading overlay
-		bsLoadingOverlayService.start({referenceId: 'user_data_selection_table'});
-		
-		// get the user data selections
-		UserDataSelection.get({}, function(user_data_selections) {
-			// update the data selections list
-			$scope.user_data_selections = user_data_selections.objects;
-			
-			// stop the loading overlay
-			bsLoadingOverlayService.stop({referenceId: 'user_data_selection_table'});
-		});
-	};
-	
-	// function to delete a specific user data selection
-	$scope.delete_user_data_selection = function(user_data_selection){
-		console.log("Deleting user data selection", user_data_selection);
-		
-		// get the user data selections
-		UserDataSelection.delete({id: user_data_selection.id}, function() {
-			// update the data selections list
-			var index = $scope.user_data_selections.indexOf(user_data_selection);
-			if (index > -1) {
-				$scope.user_data_selections.splice(index, 1);
-			}
-			else {
-				console.log("Data selection not found in list", user_data_selection);
-			}
-		});
-	};
-	
-	// load the data selection list
-	$scope.load_user_data_selections();
+	/* DEFINITIONS */
 	
 	// function to open data selection detail in modal
-	$scope.open_user_data_selection_detail = function(user_data_selection) {
+	function open_detail(data_selection) {
 		$uibModal.open({
 			templateUrl: 'data_selection/data_selection_detail.html',
 			size: 'lg',
-			controller: 'UserDataSelectionDetailController',
+			controller: 'DataSelectionDetailController',
+			controllerAs: 'data_selection_ctrl',
 			resolve: {
-				user_data_selection: function () { return user_data_selection }
+				data_selection: function () { return data_selection }
 			},
 		});
 	};
 })
-.controller('UserDataSelectionDetailController',
-function($scope, $uibModalInstance, user_data_selection) {
-	console.log("UserDataSelectionDetailController scope", $scope.$id);
-	$scope.user_data_selection = user_data_selection;
-	$scope.data_selections = user_data_selection.data_selections;
+.controller('DataSelectionDetailController',
+function($uibModalInstance, data_selection) {
+	var vm = this;
+	
+	vm.parent_data_selection = data_selection;
+	vm.data_selections = data_selection.data_selections;
 });

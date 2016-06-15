1f100704b6df0cefec5decf7d034d2b3c2c9a9a2
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-06-15 17:32:35 +0200

    deployment

--- global/module.js
+++ global/module.js
@@ -2,8 +2,10 @@ angular
 .module('global', [
 	'ngTastyResource',
 	'bsLoadingOverlay',
+	'ui.bootstrap',
+	'ui.bootstrap.datetimepicker'
 ])
-.constant('SDA_URL', 'http://solarnet.oma.be:8080/api/v1/')
+.constant('SDA_URL', 'http://solarnet.oma.be/SDA/api/v1/')
 .config(function(tastyResourceProvider, SDA_URL){
 	tastyResourceProvider.config.api_url = SDA_URL;
 	tastyResourceProvider.config.cache = true;
@@ -15,4 +17,3 @@ angular
 		templateUrl: "libs/angular-loading-overlay/angular-loading-overlay.html" // Template url for overlay element. If not specified - no overlay element is created.
 	});
 });
-

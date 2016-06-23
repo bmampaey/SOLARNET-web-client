f99a4ade94feca6a1de97526938cd82207e1eb13
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-06-23 13:15:01 +0200

    updated all librairies

--- data_selection/resources.js
+++ data_selection/resources.js
@@ -1,6 +1,6 @@
 angular.module('dataSelectionApp')
 .factory('DataSelectionGroup', function(tastyResource) {
-	return new tastyResource('data_selection_group/:id', {paramDefaults: {id: '@id'}, cache: false});
+	return new tastyResource('data_selection_group/:id', {paramDefaults: {id: '@id'}, cache: false, page_size: 0});
 })
 .factory('DataSelection', function(tastyResource) {
 	return new tastyResource('data_selection/:id', {paramDefaults: {id: '@id'}, cache: false});

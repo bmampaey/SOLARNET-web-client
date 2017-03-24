03c4a1196387355beb509e78c77fbc6c920c5542
Benjamin Mampaey <benjamin.mampaey@oma.be>
2017-03-24 16:08:20 +0100

    improved Rosa metadata

--- metadata/rosa.js
+++ metadata/rosa.js
@@ -1,7 +1,7 @@
 angular.module('metadataApp')
 .factory('rosa', function(getPropFilter){
 	
-	var wavelnth = [3500, 4170, 4300];
+	var wavelnth = [3500.0, 4170.0, 4305.5];
 	var target = ['AR 12192'];
 	
 	return {

26e41d3a71529ca815ab2843037389f5bbc00fd3
Benjamin Mampaey <benjamin.mampaey@oma.be>
2017-03-27 17:11:08 +0200

    improved xrt

--- metadata/xrt.js
+++ metadata/xrt.js
@@ -1,6 +1,13 @@
 angular.module('metadataApp')
 .factory('xrt', function(getPropFilter){
 	
+	var target = [
+		{'value': 'AR', 'name': 'Active Region'},
+		{'value': 'CH', 'name': 'Coronal Hole'},
+		{'value': 'FS', 'name': 'Flare'},
+		{'value': 'QR', 'name': 'Quiet Region'},
+	];
+	
 	return {
 		columns: [
 			['date_obs', 'Observation date'],
@@ -9,8 +16,22 @@ angular.module('metadataApp')
 		],
 		form_template_url: '/SVO/metadata/xrt.html',
 		form_config: {
-			target: ['AR', 'CH', 'FS', 'QR']
-		}
+			target: target
+		},
+		parse_search_criteria: parse_search_criteria,
 	};
+	
+	// parse search criteria into search params for the Metadata resource
+	function parse_search_criteria(search_criteria) {
+		var search_params = angular.extend({}, search_criteria);
+		// convert target object to target value
+		console.log('search_params', search_params);
+		if(search_criteria.target__in != undefined)
+		{
+			search_params.target__in = search_params.target__in.map(function(target){return target.value;});
+		}
+		console.log('search_params', search_params);
+		return search_params;
+	}
 
 });

71932c63ee17236c46c2bfb0cc70ec8e6273a7d2
Benjamin Mampaey <benjamin.mampaey@oma.be>
2021-07-20 00:30:45 +0200

    Reorganized Metadata vues with a mixin

--- src/components/metadata/AiaLevel1.vue
+++ src/components/metadata/AiaLevel1.vue
@@ -9,6 +9,11 @@
 				<b-form-group description="Only display data with a quality of 0">
 					<b-form-checkbox v-model="searchFilter.bestQuality">Best quality only</b-form-checkbox>
 				</b-form-group>
+				<b-overlay :show="showOverlay" rounded="sm">
+					<b-form-group label="Tags" label-for="tags">
+						<b-form-select id="tags" v-model="searchFilter.tags" :options="tagOptions" multiple></b-form-select>
+					</b-form-group>
+				</b-overlay>
 				<b-button type="submit" variant="primary">Search</b-button>
 			</b-form>
 		</b-col>
@@ -19,33 +24,59 @@
 </template>
 
 <script>
-import MetadataList from './MetadataList';
-import AiALev1SearchFilter from './AiaLevel1SearchFilter';
+import MetadataMixin from './MetadataMixin';
+import MetadataSearchFilter from '@/services/svo/MetadataSearchFilter';
+
+const WAVELENGTHS = [94, 131, 171, 193, 211, 304, 335, 1600, 1700, 4500];
+
+class AiALev1SearchFilter extends MetadataSearchFilter {
+	wavelengthOptions = WAVELENGTHS.map(w => ({ value: w, text: `${w} Å` }));
+	wavelengths = [];
+	bestQuality;
+
+	constructor(searchFilter) {
+		super(searchFilter);
+		if (searchFilter) {
+			if (Array.isArray(searchFilter.wavelengths)) {
+				this.wavelengths = [...searchFilter.wavelengths];
+			} else if (searchFilter.wavelengthRange) {
+				let min = searchFilter.wavelengthRange.min != null ? searchFilter.wavelengthRange.min * 10 : -Infinity;
+				let max = searchFilter.wavelengthRange.max != null ? searchFilter.wavelengthRange.max * 10 : Infinity;
+				if (isFinite(min) || isFinite(max)) {
+					this.wavelengths = WAVELENGTHS.filter(w => min <= w && w <= max);
+				}
+			}
+			this.bestQuality = searchFilter.bestQuality;
+		}
+	}
+
+	getSearchParams() {
+		let searchParams = super.getSearchParams();
+
+		for (const wavelength of this.wavelengths) {
+			searchParams.append('wavelnth__in', wavelength);
+		}
+
+		if (this.bestQuality) {
+			searchParams.set('quality', 0);
+		}
+
+		return searchParams;
+	}
+}
 
 export default {
 	name: 'AiaLevel1',
-	components: {
-		MetadataList
-	},
-	props: {
-		dataset: { type: Object, required: true },
-		initialSearchFilter: { type: Object, required: false, default: null }
-	},
+	mixins: [MetadataMixin],
 	data: function() {
 		return {
 			searchFilter: new AiALev1SearchFilter(this.initialSearchFilter),
-			searchParams: new URLSearchParams(),
 			columns: [
 				{ label: 'Observation date', key: 'date_obs', formatter: this.$utils.formatDate },
 				{ label: 'Wavelength (Å)', key: 'wavelnth' },
 				{ label: 'Quality', key: 'quality' }
 			]
 		};
-	},
-	methods: {
-		updateSearchParams: function() {
-			this.searchParams = this.searchFilter.getSearchParams();
-		}
 	}
 };
 </script>
--- src/components/metadata/Metadata.vue
+++ src/components/metadata/Metadata.vue
@@ -18,47 +18,10 @@
 </template>
 
 <script>
-import MetadataList from './MetadataList';
-import MetadataSearchFilter from '@/services/svo/MetadataSearchFilter';
+import MetadataMixin from './MetadataMixin';
 
 export default {
 	name: 'Metadata',
-	components: {
-		MetadataList
-	},
-	props: {
-		dataset: { type: Object, required: true },
-		initialSearchFilter: { type: Object, required: false, default: null }
-	},
-	data: function() {
-		return {
-			searchFilter: new MetadataSearchFilter(this.initialSearchFilter),
-			searchParams: new URLSearchParams(),
-			tagOptions: [],
-			columns: [
-				{ label: 'Observation date', key: 'date_beg', formatter: this.$utils.formatDate },
-				{ label: 'Wavelength', key: 'wavemin' }
-			]
-		};
-	},
-	created: async function() {
-		// Fetch and create the options of the form select
-		this.showOverlay = true;
-		this.loadTagOptions();
-		this.showOverlay = false;
-	},
-	methods: {
-		updateSearchParams: function() {
-			this.searchParams = this.searchFilter.getSearchParams();
-		},
-		loadTagOptions: async function() {
-			try {
-				let tags = await this.$SVO.tag.getAll({ dataset: this.dataset.name });
-				this.tagOptions = tags.map(tag => ({ value: tag.name, text: tag.name }));
-			} catch (error) {
-				this.$displayErrorMessage(this.$SVO.parseError(error));
-			}
-		}
-	}
+	mixins: [MetadataMixin]
 };
 </script>

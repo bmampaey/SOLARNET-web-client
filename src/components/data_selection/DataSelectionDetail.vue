7e3cbac38abb6855318abb45ed379bfab9da6040
Benjamin Mampaey <benjamin.mampaey@oma.be>
2021-08-31 17:55:05 +0200

    Added DataSelectionDetail

--- src/components/data_selection/DataSelectionDetail.vue
+++ src/components/data_selection/DataSelectionDetail.vue
@@ -1,14 +1,35 @@
 <template>
 	<div>
-		TODO
+		<b-card v-if="dataSelection.description" class="mb-2">
+			<b-card-text v-text="dataSelection.description"></b-card-text>
+		</b-card>
+		<component :is="metadataComponent" :dataset="dataset" :initial-search-filter="searchFilter" class="mt-3"></component>
 	</div>
 </template>
 
 <script>
+import metadataComponents from '@/components/metadata';
+
 export default {
 	name: 'DataSelectionDetail',
 	props: {
 		dataSelection: { type: Object, required: true }
+	},
+	data: function() {
+		return {
+			dataset: {
+				...this.dataSelection.dataset,
+				metadata: this.dataSelection.metadata
+			},
+			searchFilter: null
+		};
+	},
+	computed: {
+		/* Component to display the Metadata for the displayed dataset */
+		metadataComponent: function() {
+			const component = metadataComponents[this.dataset.name] || metadataComponents['default'];
+			return () => import('@/components/metadata/' + component);
+		}
 	}
 };
 </script>
